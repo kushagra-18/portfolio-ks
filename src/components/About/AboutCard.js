0cb5ea8cc596c9d19c53f65f46e421d011d0ef40
Kushagra Sharma <giantkillerkushagra@gmail.com>
2022-11-28 23:45:00 +0530

    first commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Kushagra Sharma </span>
+            from <span className="purple"> Gurugram, India.</span>
+            <br />I have completed my B.Tech in Computer Science from NCU, Gurugram.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing/Watching Football
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Reading Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Soumyajit</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Software Engineer",
+          "Deep Learning Enthusiast",
+          "DSA Enthusiast",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
